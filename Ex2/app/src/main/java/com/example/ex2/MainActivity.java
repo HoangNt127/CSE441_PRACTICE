1cf216c3cd1b7b99695a752a3a1bbd52f9fb1154
HoangNt <hoanggata123@gmail.com>
2024-10-03 19:29:19 +0700

    second commit Ex2

--- Ex2/app/src/main/java/com/example/ex2/MainActivity.java
+++ Ex2/app/src/main/java/com/example/ex2/MainActivity.java
@@ -13,12 +13,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btncall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this,"CR424 - onCreat()",Toast.LENGTH_SHORT).show(); btncall = findViewById(R.id.btncall); btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(MainActivity.this,Subactivity.class); startActivity(intent1);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- Ex2/app/src/main/java/com/example/ex2/Subactivity.java
+++ Ex2/app/src/main/java/com/example/ex2/Subactivity.java
@@ -11,12 +11,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Subactivity extends AppCompatActivity {
-
+    Button btnok;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_subactivity);
+
+        btnok = findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
