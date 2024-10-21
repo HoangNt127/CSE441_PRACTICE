60720055ad7ce298a79fef9cd9e3f8eb773df7b3
HoangNt <hoanggata123@gmail.com>
2024-10-22 00:21:54 +0700

    third commit Ex8

--- Ex8/app/src/main/java/com/example/ex8/SendSMSActivity.java
+++ Ex8/app/src/main/java/com/example/ex8/SendSMSActivity.java
@@ -1,6 +1,12 @@
 package com.example.ex8;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SendSMSActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_send_sms);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+    EditText edtsms;
+    Button btnback2;
+    ImageButton btnsendsms; @Override
+    protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); setContentView(R.layout.sms_layout);
+        edtsms = (EditText) findViewById(R.id.edtsms);
+        btnback2 = (Button) findViewById(R.id.btnback2); btnsendsms = (ImageButton) findViewById(R.id.btnsendsms); btnsendsms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callintent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+edtsms.getText().toString()));
+                startActivity(callintent);
+            }
+        });
+        btnback2.setOnClickListener(new View.OnClickListener() { @Override
+        public void onClick(View view) { finish();
+        }
         });
     }
 }
