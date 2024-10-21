33c4ea78175b46b98e339a22559077903ff3297e
HoangNt <hoanggata123@gmail.com>
2024-10-21 23:13:01 +0700

    second commit Ex6

--- Ex6/app/src/main/java/com/example/ex6/MainActivity.java
+++ Ex6/app/src/main/java/com/example/ex6/MainActivity.java
@@ -1,24 +1,72 @@
 package com.example.ex6;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+    EditText edtten,editCMND,editBosung;
+    CheckBox chkdocbao,chkdocsach,chkdoccode;
+    Button btnsend;
+    RadioGroup group; @Override
+    protected void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); setContentView(R.layout.activity_main);
+        edtten = findViewById(R.id.edtten); editCMND = findViewById(R.id.edtcmnd); editBosung = findViewById(R.id.edtbosung); chkdocbao = findViewById(R.id.chkdocbao); chkdoccode = findViewById(R.id.chkcode); chkdocsach = findViewById(R.id.chkdocsach); btnsend = findViewById(R.id.btnsend);
+        btnsend.setOnClickListener(new View.OnClickListener() { @Override
+        public void onClick(View arg0) {
+// TODO Auto-generated method stub
+            doShowInformation();
+        }
         });
+    }
+    public void doShowInformation() {
+//Kiểm tra tên hợp lệ
+        String ten = edtten.getText().toString();
+        ten = ten.trim();
+        if (ten.length() < 3) {
+            edtten.requestFocus();
+            edtten.selectAll();
+            Toast.makeText(this, "Tên phải >= 3 ký tự", Toast.LENGTH_LONG).show();
+            return;
+        }
+//kiểm tra CMND hợp lệ
+        String cmnd = editCMND.getText().toString();
+        cmnd = cmnd.trim();
+        if (cmnd.length() != 9) {
+            editCMND.requestFocus();
+            editCMND.selectAll();
+            Toast.makeText(this, "CMND phải đúng 9 ký tự", Toast.LENGTH_LONG).show();
+            return;
+        }
+//Kiểm tra bằng cấp
+        String bang = "";
+        group = findViewById(R.id.idgroup);
+        int id = group.getCheckedRadioButtonId();// Trả về Id
+        if (id == -1) {
+            Toast.makeText(this, "Phải chọn bằng cấp",
+
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        RadioButton rad = findViewById(id);
+        bang = rad.getText() + "";
+//Kiểm tra sở thích
+        String sothich = "";
+        if (chkdocbao.isChecked()) sothich += chkdocbao.getText() + "\n";
+        if (chkdocsach.isChecked()) sothich += chkdocsach.getText() + "\n";
+        if (chkdoccode.isChecked()) sothich += chkdoccode.getText() + "\n";
+        String bosung = editBosung.getText() + "";
     }
 }
